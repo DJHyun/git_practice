f752cacd505cbc0e542ba2e900a9b362eb3c6651
DJHyun <djestiny4444@naver.com>
2019-01-03 09:49:01 +0900

    first commit

---
+++ browser.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+
+# 1. 사용자의 입력을 받아 검색하기
+# keyword = input("검색어를 입력해주세요\n")
+
+# 2. 블랙핑크 모든 멤버들의 검색 페이지를 한 번에 여는 코드
+url = "https://search.daum.net/search?q="
+
+black = ["지수","제니","로제","리사"]
+
+for i in black:
+    webbrowser.open(url+i)
+
+
+
